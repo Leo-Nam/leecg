74e8144e47608d89513bc1acde6a921714962875
leo <myleobot@gmail.com>
2025-03-22 12:29:47 +0900

    회전판게임회전시간조금늘임

--- src/components/services/mealMenu/wheelSpinnerGameComp.vue
+++ src/components/services/mealMenu/wheelSpinnerGameComp.vue
@@ -63,7 +63,7 @@ export default {
     const spinWheel = () => {
       if (spinning) return;
       spinning = true;
-      spinSpeed = Math.random() * 20 + 20;		//최소 2초에서 최대 4초사이의 시간동안 회전판이 돌게함함
+      spinSpeed = Math.random() * 30 + 30;		//최소 2초에서 최대 4초사이의 시간동안 회전판이 돌게함함
       animateSpin();
     };
 
